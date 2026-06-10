9ab63ca888a1438e802f0d5ddaf9a49314c387b5
Joao-we <jm4113385@gmail.com>
2026-06-10 14:57:39 -0300

    Quarto

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 from views import *
 
+@app.route("/")
+def home():
+    return "SITE ESTA FUNCIONANDO NO RAILWAY!"
+
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
